2a82d50d64dbdf3b45be938d9dcec5df3f64dd86
Axel Durand <44839454+Plenitude-ai@users.noreply.github.com>
2021-10-14 09:53:44 +0200

    Finished part 3 : authentication of the course

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const app = express();
 
 
-const Thing = require('./models/Thing');
+// const Thing = require('./models/Thing');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require("./routes/user")
 
 
 // connection to MONGODB
@@ -32,55 +34,10 @@ app.use((req, res, next) => {
 //   PARSING REQUEST
 app.use(bodyParser.json());
 
-// POST REQUESTS
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({ ...req.body });
-    thing.save()
-      .then( () => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
 
-
-// GET requests
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
-
-// GET ALL REQUESTS
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-
-app.put("/api/stuff/:id", (req, res, next) => {
-    Thing.updateOne({ _id : req.params.id }, {...req.body, _id:req.params.id })
-        .then( () => res.status(201).json({message : "Updated !"}))
-        .catch(error => res.status(400).json({ error }))
-})
-
-app.delete("/api/stuff/:id", (req, res, next) =>{
-    Thing.deleteOne({ _id : req.params.id})
-        .then( () => res.status(201).json({message : "object deleted !"}))
-        .catch(error => res.status(400).json({ error }) )
-})
-
-
-
-
-
-
-
-// app.get("/api/stuff/:id", (req, res, next) => {
-//     Thing.findOne({ _id : req.params.id })
-//         .then(thing => res.status_code(200).json(thing))
-//         .catch(error => res.status_code(404).json({ error }))
-// })
-
+// Routes
+app.use("/api/stuff", stuffRoutes)
+app.use("/api/auth", userRoutes)
 
 
 
