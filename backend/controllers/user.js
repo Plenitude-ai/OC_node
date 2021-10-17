7c136df58b59ca85c2c636057683e5eb47603aa8
Axel Durand <44839454+Plenitude-ai@users.noreply.github.com>
2021-10-17 21:25:45 +0200

    Finished POST /signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,32 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/user.js")
+
+
+
 exports.signup = (req, res, next) => {
+
+    console.log("\nsignup ...")
+    console.log("Password : ", req.body.password);
+    bcrypt.hash(req.body.password, 10)
+        .then( hash => {const user = new User({ email: req.body.email,
+                                                password: hash});
+                        console.log("user : ", user);
+                        user.save()
+                            .then( () => { console.log("POST succes !")
+                                            res.status(201).json({ message : "user signed up !"})
+                                        }
+                            )
+                            .catch( error => { console.log( error );
+                                                res.status(400).json({ error })
+                                            }
+                            );
+                        
+                        }
+        )
+        .catch(error => {console.log(error);
+                        res.status(500).json({ error })
+                        }
+        )
 
 };
 
